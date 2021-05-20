e4e2636a26009417d6302ae4c6ff3a908081a0f4
Jamal Hussein <jamal.hussein@tietoevry.com>
2021-05-20 19:12:02 +0200

    wire the model to the api

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -7,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  model: any  = {}
-  constructor() { }
+  model: any  = {};
+  loggedIn: boolean = false;
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   login() {
-    console.log(this.model)
+    this.accountService.login(this.model).subscribe(
+      res=> {
+        console.log(res);
+        this.loggedIn = true;
+      },
+      err=> console.log(err)
+    )
   }
 
 }
