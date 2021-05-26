7a5a5e9385eaac28399dc65150c869735566b526
Jamal Hussein <jamal.hussein@aditro.com>
2021-05-27 01:32:49 +0200

    error handling ui

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { SharedModule } from './_modules/shared.module';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import {HttpClientModule} from "@angular/common/http";
+import { SharedModule } from './_modules/shared.module';
 import { BrowserModule, } from '@angular/platform-browser';
+import { ErrorInterceptor } from './_interceptors/error.interceptor';
+import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
-import { AppRoutingModule } from './app-routing.module';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { ListsComponent } from './lists/lists.component';
 import { RegisterComponent } from './register/register.component';
+import { MessagesComponent } from './messages/messages.component';
+import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
-import { ListsComponent } from './lists/lists.component';
-import { MessagesComponent } from './messages/messages.component';
-import { ToastrModule } from 'ngx-toastr';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
+import { ServerErrorComponent } from './errors/server-error/server-error.component';
 
 
 @NgModule({
@@ -27,7 +29,10 @@ import { ToastrModule } from 'ngx-toastr';
     MessagesComponent,
     RegisterComponent,
     MemberListComponent,
+    TestErrorsComponent,
     MemberDetailComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
   ],
   imports: [
     FormsModule,
@@ -37,7 +42,9 @@ import { ToastrModule } from 'ngx-toastr';
     BrowserAnimationsModule,
     SharedModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor,multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -23,10 +23,6 @@ export class NavComponent implements OnInit {
       res=> {
         console.log(res);
         this.router.navigateByUrl("/members")
-      },
-      err=> {
-        this.toastr.error(err.error);
-        console.log(err)
       }
     )
   }
