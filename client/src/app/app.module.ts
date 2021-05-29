704745d4406562065901e3a4892ee193e6d4a088
Jamal Hussein <jamal.hussein@aditro.com>
2021-05-29 13:57:44 +0200

    added an intercepter that adds the jwn token to every request going

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from './_modules/shared.module';
@@ -45,7 +46,9 @@ import { MemberCardComponent } from './members/member-card/member-card.component
     SharedModule
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor,multi: true}
+    {provide: HTTP_INTERCEPTORS,useClass: ErrorInterceptor,multi: true},
+    {provide: HTTP_INTERCEPTORS,useClass: JwtInterceptor,multi: true}
+
   ],
   bootstrap: [AppComponent]
 })
