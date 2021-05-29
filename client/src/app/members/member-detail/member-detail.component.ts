bcc8bb042e0095fe0f3e578dc1c6cf8390efb38c
Jamal Hussein <jamal.hussein@aditro.com>
2021-05-30 01:44:20 +0200

    design the member detailed page

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,15 +1,26 @@
+import { MemberService } from './../../_services/member.service';
 import { Component, OnInit } from '@angular/core';
+import { Member } from 'src/app/_models/member';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-member-detail',
   templateUrl: './member-detail.component.html',
-  styleUrls: ['./member-detail.component.css']
+  styleUrls: ['./member-detail.component.css'],
 })
 export class MemberDetailComponent implements OnInit {
+  member: Member;
 
-  constructor() { }
+  constructor(
+    private memberService: MemberService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    this.loadMember();
   }
 
+  loadMember() {
+    this.memberService.getMember(this.route.snapshot.paramMap.get("username")).subscribe(member=> this.member = member);
+  }
 }
