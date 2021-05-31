c19481ffc886cb34014da811d77ae96e3b330527
Jamal Hussein <jamal.hussein@aditro.com>
2021-05-31 15:20:58 +0200

    update the update member in the component

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -10,38 +10,45 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-member-edit',
   templateUrl: './member-edit.component.html',
-  styleUrls: ['./member-edit.component.css']
+  styleUrls: ['./member-edit.component.css'],
 })
 export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
   // access the browser before refresh and close
-  @HostListener('window:beforeunload',['$event']) unloadNotification($event:any){
-    if(this.editForm.dirty){
+  @HostListener('window:beforeunload', ['$event']) unloadNotification(
+    $event: any
+  ) {
+    if (this.editForm.dirty) {
       $event.returnValue = true;
     }
   }
 
   constructor(
-    private accountService:AccountService,
+    private accountService: AccountService,
     private memberService: MemberService,
     private toastr: ToastrService
-    ) { 
-    this.accountService.currentUser$.pipe(take(1)).subscribe(user=> this.user = user);
+  ) {
+    this.accountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
   }
 
   ngOnInit(): void {
     this.loadMember();
   }
 
-  loadMember () {
-    this.memberService.getMember(this.user.username).subscribe(member=> this.member = member);
+  loadMember() {
+    this.memberService
+      .getMember(this.user.username)
+      .subscribe((member) => (this.member = member));
   }
 
-  updateMember(){
-    console.log(this.member)
-    this.toastr.success('profile updated successfully')
-    this.editForm.reset(this.member);
+  updateMember() {
+    this.memberService.updateMember(this.member).subscribe(() => {
+      this.toastr.success('profile updated successfully');
+      this.editForm.reset(this.member);
+    });
   }
 }
