b264eeeecc2246475018fa6c7727ad068d9ddc69
Jamal Hussein <jamal.hussein@aditro.com>
2021-06-02 00:41:54 +0200

    added upload photo functionality

---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,55 @@
+import { Photo } from './../../_models/photo';
+import { AccountService } from './../../_services/account.service';
+import { environment } from './../../../environments/environment';
+import { Component, Input, OnInit } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { Member } from 'src/app/_models/member';
+import { User } from 'src/app/_models/user';
+import { take } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.css'],
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() member: Member;
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  user: User;
+  constructor(private AccountService: AccountService) {
+    this.AccountService.currentUser$
+      .pipe(take(1))
+      .subscribe((user) => (this.user = user));
+  }
+
+  ngOnInit(): void {
+    this.initializeUploader();
+  }
+
+  fileOverBase(e: any) {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUploader() {
+    this.uploader = new FileUploader({
+      url: `${this.baseUrl}users/add-photo`,
+      authToken: `Bearer ${this.user.token}`,
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024,
+    });
+    this.uploader.onAfterAddingFile = (file) => {
+      file.withCredentials = false;
+    };
+    this.uploader.onSuccessItem = (item, response, status, headers) => {
+      if (response) {
+        var photo = JSON.parse(response);
+        this.member.photos.push(photo);
+      }
+    };
+  }
+}
