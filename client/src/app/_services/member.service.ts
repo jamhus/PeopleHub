0b7973c73955f5591df0233a02fa1f03b1ab850e
Jamal Hussein <jamal.hussein@aditro.com>
2021-06-07 11:18:26 +0200

    added pagination to the frontend

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -1,6 +1,7 @@
+import { PaginatedResult } from './../_modules/Pagination';
 import { map } from 'rxjs/operators';
 import { Member } from './../_models/member';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { of } from 'rxjs';
@@ -11,22 +12,30 @@ import { of } from 'rxjs';
 export class MemberService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  paginatedResult: PaginatedResult<Member[]> = new PaginatedResult<Member[]>();
 
   constructor(private http: HttpClient) {}
 
-  getMembers() {
-    if (this.members.length > 0) return of(this.members);
-    return this.http.get<Member[]>(`${this.baseUrl}users/`).pipe(
-      map((members) => {
-        this.members = members;
-        return members;
+  getMembers(page?: number, itemsPerPage?: number) {
+    let params = new HttpParams();
+    if (page != null && itemsPerPage != null) {
+      params = params.append('pageNumber', page.toString());
+      params = params.append('pageSize', itemsPerPage.toString());
+    }
+    return this.http.get<Member[]>(`${this.baseUrl}users/`,{observe: 'response',params}).pipe(
+      map(response => {
+        this.paginatedResult.result = response.body;
+        if(response.headers.get('Pagination')!==null){
+          this.paginatedResult.pagination = JSON.parse(response.headers.get('Pagination'));
+        }
+        return this.paginatedResult;
       })
     );
   }
 
   getMember(username: string) {
-    const member = this.members.find(x=>x.userName === username);
-    if(member!==undefined){
+    const member = this.members.find((x) => x.userName === username);
+    if (member !== undefined) {
       return of(member);
     }
     return this.http.get<Member>(`${this.baseUrl}users/${username}`);
@@ -34,18 +43,18 @@ export class MemberService {
 
   updateMember(member: Member) {
     return this.http.put(`${this.baseUrl}users`, member).pipe(
-      map(()=> {
+      map(() => {
         const index = this.members.indexOf(member);
         this.members[index] = member;
       })
     );
   }
 
-  setMainPhoto(photoId: number){
-    return this.http.put(`${this.baseUrl}users/set-main-photo/${photoId}`,{});
+  setMainPhoto(photoId: number) {
+    return this.http.put(`${this.baseUrl}users/set-main-photo/${photoId}`, {});
   }
 
-  deletePhoto(photoId: number){
-    return this.http.delete(`${this.baseUrl}users/delete-photo/${photoId}`,{});
+  deletePhoto(photoId: number) {
+    return this.http.delete(`${this.baseUrl}users/delete-photo/${photoId}`, {});
   }
 }
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { MemberService } from './../../_services/member.service';
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_modules/Pagination';
 
 @Component({
   selector: 'app-member-list',
@@ -9,10 +10,25 @@ import { Member } from 'src/app/_models/member';
   styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  members: Member[];
+  pagination: Pagination;
+  pageNumber =1;
+  pageSize =5;
   constructor(private memberService: MemberService) {}
 
   ngOnInit(): void {
-    this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers(){
+    this.memberService.getMembers(this.pageNumber,this.pageSize).subscribe(response=> {
+      this.members = response.result;
+      this.pagination = response.pagination;
+    })
+  }
+
+  pageChanged(event: any){
+    this.pageNumber = event.page;
+    this.loadMembers();
   }
 }
