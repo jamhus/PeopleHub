7ccf801509f7fbddaf0f7a2a0347a323a5a8d660
Jamal Hussein <jamal.hussein@aditro.com>
2021-06-09 23:15:09 +0200

    improve cache to keep the filters and the data even when switching between details and list

--- client/src/app/_services/member.service.ts
+++ client/src/app/_services/member.service.ts
@@ -1,11 +1,13 @@
+import { User } from 'src/app/_models/user';
+import { AccountService } from './account.service';
 import { UserParams } from './../_models/userParams';
 import { PaginatedResult } from './../_modules/Pagination';
-import { map, retryWhen } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { Member } from './../_models/member';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { of, pipe } from 'rxjs';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -14,8 +16,30 @@ export class MemberService {
   baseUrl = environment.apiUrl;
   members: Member[] = [];
   memberCache = new Map();
+  user: User;
+  userParams: UserParams;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private accountService: AccountService
+  ) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe((user) => {
+      this.user = user;
+      this.userParams = new UserParams(user);
+    });
+  }
+
+  setUserParams(params: UserParams) {
+    this.userParams = params;
+  }
+  getUserParams() {
+    return this.userParams;
+  }
+
+  resetUserParams() {
+    this.userParams = new UserParams(this.user);
+    return this.userParams;
+  }
 
   getMembers(userParams: UserParams) {
     var response = this.memberCache.get(Object.values(userParams).join('-'));
@@ -67,7 +91,7 @@ export class MemberService {
     const member = [...this.memberCache.values()]
       .reduce((arr, elem) => arr.concat(elem.result), [])
       .find((member: Member) => member.userName === username);
-      if (member) return of(member);
+    if (member) return of(member);
     return this.http.get<Member>(`${this.baseUrl}users/${username}`);
   }
 
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,10 +1,8 @@
-import { AccountService } from './../../_services/account.service';
 import { UserParams } from './../../_models/userParams';
 import { MemberService } from './../../_services/member.service';
 import { Component, OnInit } from '@angular/core';
 import { Member } from 'src/app/_models/member';
 import { Pagination } from 'src/app/_modules/Pagination';
-import { take } from 'rxjs/operators';
 import { User } from 'src/app/_models/user';
 
 @Component({
@@ -16,33 +14,35 @@ export class MemberListComponent implements OnInit {
   members: Member[];
   pagination: Pagination;
   userParams: UserParams;
-  user:User;
-  genderList = [{value: "male",display:"Males"},{value: "female",display:"Females"}]
-  constructor(private memberService: MemberService, private accoutService:AccountService) {
-    this.accoutService.currentUser$.pipe(take(1)).subscribe(user=>{
-      this.user = user;
-      this.userParams = new UserParams(user);
-    })
+  user: User;
+  genderList = [
+    { value: 'male', display: 'Males' },
+    { value: 'female', display: 'Females' },
+  ];
+  constructor(private memberService: MemberService) {
+    this.userParams = this.memberService.getUserParams();
   }
 
   ngOnInit(): void {
     this.loadMembers();
   }
 
-  loadMembers(){
-    this.memberService.getMembers(this.userParams).subscribe(response=> {
+  loadMembers() {
+    this.memberService.setUserParams(this.userParams);
+    this.memberService.getMembers(this.userParams).subscribe((response) => {
       this.members = response.result;
       this.pagination = response.pagination;
-    })
+    });
   }
 
-  resetFilters () {
-    this.userParams = new UserParams(this.user);
+  resetFilters() {
+    this.userParams = this.memberService.resetUserParams();
     this.loadMembers();
   }
 
-  pageChanged(event: any){
+  pageChanged(event: any) {
     this.userParams.pageNumber = event.page;
+    this.memberService.setUserParams(this.userParams);
     this.loadMembers();
   }
 }
