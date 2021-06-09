1c4f91cdf4d91eaa4a3c7e8b95a98023145006f7
Jamal Hussein <jamal.hussein@aditro.com>
2021-06-09 23:25:52 +0200

    added like entity

---
+++ API/Models/UserLike.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class UserLike
+    {
+        public ApplicationUser SourceUser { get; set; } // the user who is liking
+        public int SourceUserId { get; set; }
+
+        public ApplicationUser LikedUser { get; set; }
+        public int LikedUserId { get; set; }
+
+    }
+}
