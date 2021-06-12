255ee434b7361d9f542c6a315345929981df3c8d
Jamal Hussein <jamal.hussein@aditro.com>
2021-06-12 03:28:29 +0200

    configure automapper to map from message to dto

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -17,7 +17,11 @@ namespace API.Helpers
             CreateMap<Photo, PhotoDto>();
             CreateMap<MemberUpdateDto, ApplicationUser>();
             CreateMap<RegisterDto, ApplicationUser>();
-
+            CreateMap<Message, MessageDto>()
+                .ForMember(p => p.SenderPhotoUrl,
+                    opt => opt.MapFrom(src => src.Sender.Photos.FirstOrDefault(x => x.IsMain == true).Url))
+                .ForMember(p => p.RecipientPhotoUrl,
+                    opt => opt.MapFrom(src => src.Recipient.Photos.FirstOrDefault(x => x.IsMain == true).Url));
         }
     }
 }
