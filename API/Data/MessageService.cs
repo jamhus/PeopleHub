79bc02a98308e5a7e18fe13378c688cdcf6396c8
Jamal Hussein <jamal.hussein@aditro.com>
2021-06-12 03:15:02 +0200

    implement message entity and message service

---
+++ API/Data/MessageService.cs
@@ -0,0 +1,51 @@
+﻿using API.DTOS;
+using API.Helpers;
+using API.Interfaces;
+using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class MessageService : IMessageService
+    {
+        private readonly DataContext _context;
+
+        public MessageService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void AddMessage(Message message)
+        {
+            _context.Messages.Add(message);
+        }
+
+        public void DeleteMessage(Message message)
+        {
+            _context.Messages.Remove(message);
+        }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FindAsync(id);
+        }
+
+        public async Task<PagedList<MessageDto>> GetMessagesForUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 1;
+        }
+    }
+}
---
+++ API/Interfaces/IMessageService.cs
@@ -0,0 +1,20 @@
+﻿using API.DTOS;
+using API.Helpers;
+using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Interfaces
+{
+    public interface IMessageService
+    {
+        void AddMessage(Message message);
+        void DeleteMessage(Message message);
+        Task<Message> GetMessage(int id);
+        Task<PagedList<MessageDto>> GetMessagesForUser();
+        Task<IEnumerable<MessageDto>> GetMessageThread(int currentUserId, int recipientId);
+        Task<bool> SaveAllAsync();
+    }
+}
