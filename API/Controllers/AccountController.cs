c06cc763f5eb9d6531724a6377a682595ae7bdc0
Jamal Hussein <jamal.hussein@aditro.com>
2021-07-09 16:48:51 +0200

    handle login and register by identity

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,27 +1,27 @@
-﻿using API.Data;
-using API.DTOS;
+﻿using API.DTOS;
 using API.Interfaces;
 using API.Models;
 using AutoMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace API.Controllers
 {
     public class AccountController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly ITokenService _token;
         private readonly IMapper _mapper;
 
 
-        public AccountController(DataContext context, ITokenService token, IMapper mapper)
+        public AccountController( UserManager<ApplicationUser> userManager,SignInManager<ApplicationUser> signInManager, ITokenService token, IMapper mapper)
         {
-            _context = context;
+            _userManager = userManager;
+            _signInManager = signInManager;
             _token = token;
             _mapper = mapper;
         }
@@ -36,15 +36,17 @@ namespace API.Controllers
             var user = _mapper.Map<ApplicationUser>(registerDto);
 
             user.UserName = registerDto.Username.ToLower();
-        
 
-            _context.Users.Add(user);
-            await _context.SaveChangesAsync();
+
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+
+            if (!result.Succeeded) return BadRequest(result.Errors);
 
             return new UserDto
             {
                 Username = user.UserName,
                 Token = _token.CreateToken(user),
+                PhotoUrl = user.Photos.FirstOrDefault(x=>x.IsMain)?.Url,
                 KnownAs = user.KnownAs,
                 Gender = user.Gender
             };
@@ -53,10 +55,14 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _context.Users
+            var user = await _userManager.Users
                 .Include(p => p.Photos)
-                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+                .SingleOrDefaultAsync(x => x.UserName == loginDto.Username.ToLower());
             if (user == null) return Unauthorized("Invalid username");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+            if (!result.Succeeded) return Unauthorized();
 
             return new UserDto
             {
@@ -70,7 +76,7 @@ namespace API.Controllers
 
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
+            return await _userManager.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
