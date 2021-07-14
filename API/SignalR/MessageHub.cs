a8a0e5935c40cecb26e1fc5d245dafa84eec1165
Jamal Hussein <jamal.hussein@aditro.com>
2021-07-14 22:49:56 +0200

    created messagehub

---
+++ API/SignalR/MessageHub.cs
@@ -0,0 +1,44 @@
+﻿using API.Extensions;
+using API.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace API.SignalR
+{
+    public class MessageHub : Hub
+    {
+        private readonly IMessageService _messageService;
+        private readonly IMapper _mapper;
+
+        public MessageHub(IMessageService messageService, IMapper mapper)
+        {
+            _messageService = messageService;
+            _mapper = mapper;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            var httpContext = Context.GetHttpContext();
+            var otherUser = httpContext.Request.Query["user"].ToString();
+            var groupName = GetGroupName(Context.User.GetUserName(), otherUser);
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
+
+            var messages = await _messageService.GetMessageThread(Context.User.GetUserName(), otherUser);
+
+            await Clients.Group(groupName).SendAsync("ReciveMessageThread", messages);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+
+        private string GetGroupName(string caller,string other)
+        {
+            var stringCompare = string.CompareOrdinal(caller, other) < 0;
+            return stringCompare ? $"{caller}-{other}" : $"{other}-{caller}";
+        }
+    }
+}
